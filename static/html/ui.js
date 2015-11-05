245c55612cd2317bd8c6cf0a95bc4d82366e6c9d
zelon <zelonion@gmail.com>
2015-11-05 23:58:37 +0900

    속도 조절 기능을 버튼으로 만듦. 전체 화면 레이아웃 조정. ui.js 파일 추가

---
+++ static/html/ui.js
@@ -0,0 +1,31 @@
+
+function make_rate_buttons() {
+  var rate_div = document.getElementById("rate");
+	var rate_list = player.getAvailablePlaybackRates();
+  var html = "속도 조절 :";
+
+  if (rate_list.length == 0) {
+    rate_div.innerHTML = html + " 불가능";
+    return;
+  }
+  for (var i=0; i<rate_list.length; ++i) {
+    var speed = rate_list[i];
+    html += "&nbsp;<button onclick='set_rate(" + speed + ");'>" + speed + "</button>";
+  }
+  rate_div.innerHTML = html;
+}
+
+function set_rate(speed) {
+  player.setPlaybackRate(speed);
+}
+function onUrl() {
+	var url = document.getElementById("url").value;
+	player.loadVideoByUrl("http://www.youtube.com/v/xruZHVN8OZ8?version=3");
+}
+
+function options() {
+	var option_list = player.getOptions();
+	var get_option = "fontSize";
+	var caption = player.getOption("captions", get_option);
+	//player.setPlaybackRate(0.5);
+}
