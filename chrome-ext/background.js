1c2729911937999314c03801df8bc449d9b8bf46
zelon <zelonion@gmail.com>
2015-11-09 00:08:31 +0900

    클릭하면 현재 url 을 가지고 youtube english 로 보내는 크롬 확장 구현

---
+++ chrome-ext/background.js
@@ -0,0 +1,5 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	var current_url = tab.url;
+  var target_url = "http://youtube-english-1118.appspot.com/?q=" + current_url;
+  chrome.tabs.update(tab.id, { url: target_url });
+});
